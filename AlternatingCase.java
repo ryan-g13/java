c72d587c8dfc88509e8dc125314f767b5a96dc7e
Ryan Groesch <ryan.groesch@gmail.com>
2019-07-29 21:41:01 -0700

    Starting code challenge

---
+++ AlternatingCase.java
@@ -0,0 +1,11 @@
+public class StringUtils {
+  
+  public static String toAlternativeString(String string) {
+    // Algorithm: 
+    // Create var to store array of chars, create var for answer string 
+    // split string into array
+    // iterate through string to check each char if caps or small case
+    // flip the case and concatenate to the answer string
+    // return the string
+  }
+}
